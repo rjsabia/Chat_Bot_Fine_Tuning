49f4d19943e3424fab46c3f88de941034314a0bb
Russell Sabia <rjsabia@gmail.com>
2023-05-19 17:01:57 -0500

    have basic functionalty working, can pull into List form data and send api call to OpenAi

--- main.py
+++ main.py
@@ -2,6 +2,48 @@ import os
 import openai
 import requests
 
+import pandas as pd
+
 # Environmental variables
-openai.api_key = os.getenv('OPEN_AI_KEY_01')
+
+# OLD KEY NOT Working
+# openai.api_key = os.getenv('OPEN_AI_KEY_01')
+openai.api_key = os.getenv('OPEN_AI_KEY_03')
+
+
+
+# --------------------------------------------
+
+qa_df = pd.read_csv("python_qa.csv")
+
+questions , answers = qa_df['Body'] , qa_df['Answer']
+qa_openai_format =  [ {"prompt":q, "completion":a} for q,a in zip(questions,answers)]
+
+# testing output
+# print(qa_openai_format[4])
+# qa_openai_format[4]['prompt']
+
+
+response = openai.Completion.create(
+            model="text-babbage-001",
+            prompt = qa_openai_format[4]['prompt'],
+            max_tokens = 250,
+            temperature = 0
+)
+
+# response['choices'][0]['text']
+print(response['choices'][0]['text'])
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import os
 import openai
 
-openai.api_key = os.getenv('OPEN_AI_KEY_01')
+# openai.api_key = os.getenv('OPEN_AI_KEY_01')
+openai.api_key = os.getenv('OPEN_AI_KEY_03')
+
 
 prompt1 = "Give me details about the technology startup called Mimi and Pimo"
 prompt2 = "Give me details about the technology startup called Mimi and Pimo. Only answer if you are 100% sure that this company exist, otherwise specify, 'I don't know man'"
@@ -13,3 +15,5 @@ response = openai.Completion.create(engine='text-davinci-003',
 
 test = response['choices'][0]['text']
 print(test)
+
+
